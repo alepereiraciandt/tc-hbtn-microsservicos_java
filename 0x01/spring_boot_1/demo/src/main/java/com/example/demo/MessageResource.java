e6df4f685d371dd244b131b8dafbd9e848877671
alepereiraciandt <alexandrepereira@ciandt.com>
2022-06-30 10:18:15 -0300

    feat: spring_boot_1

---
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/messages")
+public class MessageResource {
+
+    @GetMapping
+    public String getHome() {
+        return "THIS IS THE HOME";
+    }
+
+    @GetMapping(value = "/word")
+    public String getHello() {
+        return "Hello World";
+    }
+
+    @GetMapping(value = "/word/{nome}")
+    public String getSalute(@PathVariable String nome) {
+        return "Hello World " + nome.toUpperCase();
+    }
+
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+        return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+
+    @GetMapping("/login/{user}/{password}")
+    public String login(@PathVariable String user, @PathVariable String password) {
+
+        if (user.isEmpty() || password.isEmpty())
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+
+        if (user.length() > 15 || password.length() > 15)
+            return "USUÁRIO E SENHA INVÁLIDOS";
+
+        return "LOGIN EFETUADO COM SUCESSO !!!";
+    }
+}
